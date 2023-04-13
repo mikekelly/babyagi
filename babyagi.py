c9175e19a344d4ceeaa5000e32a74ed24315291b
Mike Kelly <mikekelly321@gmail.com>
2023-04-13 01:37:54 +0100

    clean up code a bit

--- babyagi.py
+++ babyagi.py
@@ -13,93 +13,9 @@ from langchain.docstore import InMemoryDocstore
 from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
 from langchain.chat_models import ChatOpenAI
 import faiss
+import utils
 
 load_dotenv()
-
-def get_env_var(key: str, error_msg: str) -> str:
-    value = os.getenv(key, "")
-    if not value:
-        raise ValueError(f"{error_msg} environment variable is missing from .env")
-    return value
-
-OPENAI_API_KEY = get_env_var("OPENAI_API_KEY", "OPENAI_API_KEY")
-MAX_ITERATIONS = get_env_var("MAX_ITERATIONS", "MAX_ITERATIONS")
-SERPAPI_API_KEY = get_env_var("SERPAPI_API_KEY", "SERPAPI_API_KEY")
-OBJECTIVE = get_env_var("OBJECTIVE", "OBJECTIVE")
-FIRST_TASK = get_env_var("FIRST_TASK", "FIRST_TASK")
-
-# Define your embedding model
-embeddings_model = OpenAIEmbeddings()
-# Initialize the vectorstore as empty
-
-embedding_size = 1536
-index = faiss.IndexFlatL2(embedding_size)
-vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
-
-
-todo_prompt = PromptTemplate.from_template("You are a planner who is an expert at coming up with a todo list for a given objective. Come up with a todo list for this objective: {objective}")
-todo_chain = LLMChain(llm=OpenAI(temperature=0), prompt=todo_prompt)
-
-search = SerpAPIWrapper()
-tools = [
-    Tool(
-        name = "Search",
-        func=search.run,
-        description="useful for when you need to answer questions about current events"
-    ),
-    Tool(
-        name = "TODO",
-        func=todo_chain.run,
-        description="useful for when you need to come up with todo lists. Input: an objective to create a todo list for. Output: a todo list for that objective. Please be very clear what the objective is!"
-    )
-]
-
-prefix = """You are an AI who performs one task based on the following objective: {objective}. Take into account these previously completed tasks: {context}."""
-suffix = """Question: {task}
-{agent_scratchpad}"""
-prompt = ZeroShotAgent.create_prompt(
-    tools, 
-    prefix=prefix, 
-    suffix=suffix, 
-    input_variables=["objective", "task", "context","agent_scratchpad"]
-)
-
-def get_next_task(task_creation_chain: LLMChain, result: Dict, task_description: str, task_list: List[str], objective: str) -> List[Dict]:
-    """Get the next task."""
-    incomplete_tasks = ", ".join(task_list)
-    response = task_creation_chain.run(result=result, task_description=task_description, incomplete_tasks=incomplete_tasks, objective=objective)
-    new_tasks = response.split('\n')
-    return [{"task_name": task_name} for task_name in new_tasks if task_name.strip()]
-
-def prioritize_tasks(task_prioritization_chain: LLMChain, this_task_id: int, task_list: List[Dict], objective: str) -> List[Dict]:
-    """Prioritize tasks."""
-    task_names = [t["task_name"] for t in task_list]
-    next_task_id = int(this_task_id) + 1
-    response = task_prioritization_chain.run(task_names=task_names, next_task_id=next_task_id, objective=objective)
-    new_tasks = response.split('\n')
-    prioritized_task_list = []
-    for task_string in new_tasks:
-        if not task_string.strip():
-            continue
-        task_parts = task_string.strip().split(".", 1)
-        if len(task_parts) == 2:
-            task_id = task_parts[0].strip()
-            task_name = task_parts[1].strip()
-            prioritized_task_list.append({"task_id": task_id, "task_name": task_name})
-    return prioritized_task_list
-
-def _get_top_tasks(vectorstore, query: str, k: int) -> List[str]:
-    """Get the top k tasks based on the query."""
-    results = vectorstore.similarity_search_with_score(query, k=k)
-    if not results:
-        return []
-    sorted_results, _ = zip(*sorted(results, key=lambda x: x[1], reverse=True))
-    return [str(item.metadata['task']) for item in sorted_results]
-
-def execute_task(vectorstore, execution_chain: LLMChain, objective: str, task: str, k: int = 5) -> str:
-    """Execute a task."""
-    context = _get_top_tasks(vectorstore, query=objective, k=k)
-    return execution_chain.run(objective=objective, context=context, task=task)
 
 class TaskCreationChain(LLMChain):
     """Chain to generates tasks."""
@@ -154,7 +70,7 @@ class BabyAGI(Chain, BaseModel):
     task_id_counter: int = Field(1)
     vectorstore: VectorStore = Field(init=False)
     max_iterations: Optional[int] = None
-        
+
     class Config:
         """Configuration for this pydantic object."""
         arbitrary_types_allowed = True
@@ -174,11 +90,11 @@ class BabyAGI(Chain, BaseModel):
     def print_task_result(self, result: str):
         print("\033[93m\033[1m" + "\n*****TASK RESULT*****\n" + "\033[0m\033[0m")
         print(result)
-        
+
     @property
     def input_keys(self) -> List[str]:
         return ["objective"]
-    
+
     @property
     def output_keys(self) -> List[str]:
         return []
@@ -198,7 +114,7 @@ class BabyAGI(Chain, BaseModel):
                 self.print_next_task(task)
 
                 # Step 2: Execute the task
-                result = execute_task(
+                result = utils.execute_task(
                     self.vectorstore, self.execution_chain, objective, task["task_name"]
                 )
                 this_task_id = int(task["task_id"])
@@ -213,7 +129,7 @@ class BabyAGI(Chain, BaseModel):
                 )
 
                 # Step 4: Create new tasks and reprioritize task list
-                new_tasks = get_next_task(
+                new_tasks = utils.get_next_task(
                     self.task_creation_chain, result, task["task_name"], [t["task_name"] for t in self.task_list], objective
                 )
                 for new_task in new_tasks:
@@ -221,7 +137,7 @@ class BabyAGI(Chain, BaseModel):
                     new_task.update({"task_id": self.task_id_counter})
                     self.add_task(new_task)
                 self.task_list = deque(
-                    prioritize_tasks(
+                    utils.prioritize_tasks(
                         self.task_prioritization_chain, this_task_id, list(self.task_list), objective
                     )
                 )
@@ -257,6 +173,48 @@ class BabyAGI(Chain, BaseModel):
             vectorstore=vectorstore,
             **kwargs
         )
+
+OPENAI_API_KEY = utils.get_env_var("OPENAI_API_KEY", "OPENAI_API_KEY")
+MAX_ITERATIONS = utils.get_env_var("MAX_ITERATIONS", "MAX_ITERATIONS")
+SERPAPI_API_KEY = utils.get_env_var("SERPAPI_API_KEY", "SERPAPI_API_KEY")
+OBJECTIVE = utils.get_env_var("OBJECTIVE", "OBJECTIVE")
+FIRST_TASK = utils.get_env_var("FIRST_TASK", "FIRST_TASK")
+
+# Define your embedding model
+embeddings_model = OpenAIEmbeddings()
+# Initialize the vectorstore as empty
+
+embedding_size = 1536
+index = faiss.IndexFlatL2(embedding_size)
+vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+
+todo_prompt = PromptTemplate.from_template("You are a planner who is an expert at coming up with a todo list for a given objective. Come up with a todo list for this objective: {objective}")
+todo_chain = LLMChain(llm=OpenAI(temperature=0), prompt=todo_prompt)
+
+search = SerpAPIWrapper()
+tools = [
+    Tool(
+        name = "Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events"
+    ),
+    Tool(
+        name = "TODO",
+        func=todo_chain.run,
+        description="useful for when you need to come up with todo lists. Input: an objective to create a todo list for. Output: a todo list for that objective. Please be very clear what the objective is!"
+    )
+]
+
+prefix = """You are an AI who performs one task based on the following objective: {objective}. Take into account these previously completed tasks: {context}."""
+suffix = """Question: {task}
+{agent_scratchpad}"""
+prompt = ZeroShotAgent.create_prompt(
+    tools,
+    prefix=prefix,
+    suffix=suffix,
+    input_variables=["objective", "task", "context","agent_scratchpad"]
+)
+
 
 llm = OpenAI(temperature=0)
 # Logging of LLMChains
